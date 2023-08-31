77e2773616cdf7b544b199f40c82f50510b39830
Dracon-151 <demianalexisalvarado@gmail.com>
2023-08-30 22:49:38 -0700

    Cambiar layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
-<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="enable">
+<html lang="en" data-layout="horizontal" data-topbar="light" data-sidebar="dark" 
+data-sidebar-size="lg" data-sidebar-image="none" data-preloader="enable" data-layout-width="boxed">
 
 <head>
     <x-head>
