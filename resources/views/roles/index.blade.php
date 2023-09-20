4cb02fb77cf5e8ed42315de2e547b6f7ec4efc16
Luis Pablo <racheth14@gmail.com>
2023-09-19 18:01:20 -0700

    Detalle profile

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,8 +1,13 @@
 <x-app :breadcrumb="$breadcrumb_info"> 
-    <roles-index
+    <!-- <roles-index
         :variables='@json(get_defined_vars())'
-    ></roles-index>
+    ></roles-index> -->
 
+    <roles-index
+        :roles = '@json(get_defined_vars())'
+        :permissions = '@json($permissions)'
+    ></roles-index>
+    
     <x-slot name="scripts">
         <script src="{{ asset('assets/libs/list.js/list.min.js') }}"></script>
         <script src="{{ asset('assets/libs/list.pagination.js/list.pagination.min.js') }}"></script>
