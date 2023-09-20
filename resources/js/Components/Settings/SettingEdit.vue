42af03c5e3bf58bab8c99d827cdcfe87e0f9038e
Sergio Galicia <pandorum117@gmail.com>
2023-09-20 00:50:33 -0700

    modificacion en settings, no funcional

---
+++ resources/js/Components/Settings/SettingEdit.vue
@@ -0,0 +1,134 @@
+<template>
+    <app-modal
+      :modal_id="modal_id"
+      base_route="settings"
+      :form="form"
+      :validator="validator"
+      :title="title_modal"
+      :method_edit="method_edit"
+      @onSubmit="methods.submitForm($event)"
+    >
+
+    <!-- Campo para Google reCAPTCHA Public Key -->
+    <basic-input
+      v-model:keyValue="form.google_recaptcha_public_key"
+      title="Google reCAPTCHA Public Key"
+      inputName="google_recaptcha_public_key"
+      type="text"
+      :cols="12"
+      :errors="validator.google_recaptcha_public_key?.$errors"
+    ></basic-input>
+
+    <!-- Campo para Stripe Production Public Key -->
+    <basic-input
+      v-model:keyValue="form.strip_production_public_key"
+      title="Stripe Production Public Key"
+      inputName="strip_production_public_key"
+      type="text"
+      :cols="12"
+      :errors="validator.strip_production_public_key?.$errors"
+    ></basic-input>
+
+    <!-- Campo para Stripe Test Public Key -->
+    <basic-input
+      v-model:keyValue="form.strip_test_public_key"
+      title="Stripe Test Public Key"
+      inputName="strip_test_public_key"
+      type="text"
+      :cols="12"
+      :errors="validator.strip_test_public_key?.$errors"
+    ></basic-input>
+
+    <!-- Campo para el valor en dólares -->
+    <basic-input
+      v-model:keyValue="form.dollar_value"
+      title="Valor en Dólares"
+      inputName="dollar_value"
+      type="number"
+      :cols="12"
+      :errors="validator.dollar_value?.$errors"
+    ></basic-input>
+
+    <!-- Campo para el valor en euros -->
+    <basic-input
+      v-model:keyValue="form.euro_value"
+      title="Valor en Euros"
+      inputName="euro_value"
+      type="number"
+      :cols="12"
+      :errors="validator.euro_value?.$errors"
+    ></basic-input>
+
+    <!-- Campo para el correo electrónico de notificaciones -->
+    <basic-input
+      v-model:keyValue="form.notification_email"
+      title="Correo Electrónico de Notificaciones"
+      inputName="notification_email"
+      type="email"
+      :cols="12"
+      :errors="validator.notification_email?.$errors"
+    ></basic-input>
+
+    <!-- Campo para reCAPTCHA Private Key -->
+    <basic-input
+      v-model:keyValue="form.google_recaptcha_private_key"
+      title="Google reCAPTCHA Private Key"
+      inputName="google_recaptcha_private_key"
+      type="text"
+      :cols="12"
+      :errors="validator.google_recaptcha_private_key?.$errors"
+    ></basic-input>
+
+    <!-- Campo para Stripe Production Private Key -->
+    <basic-input
+      v-model:keyValue="form.strip_production_private_key"
+      title="Stripe Production Private Key"
+      inputName="strip_production_private_key"
+      type="text"
+      :cols="12"
+      :errors="validator.strip_production_private_key?.$errors"
+    ></basic-input>
+
+    <!-- Campo para Stripe Test Private Key -->
+    <basic-input
+      v-model:keyValue="form.strip_test_private_key"
+      title="Stripe Test Private Key"
+      inputName="strip_test_private_key"
+      type="text"
+      :cols="12"
+      :errors="validator.strip_test_private_key?.$errors"
+    ></basic-input>
+
+</app-modal>
+
+</template>
+
+<script>
+import BtnOption from '@/Components/BtnOption.vue'
+import BasicInput from '@/Components/BasicInput.vue'
+export default {
+    components: {
+        BtnOption,
+        BasicInput
+    },
+    props:{
+       
+    },
+    setup(props){
+        const form = {
+        google_recaptcha_public_key: '',
+        strip_production_public_key: '',
+        strip_test_public_key: '',
+        dollar_value: '',
+        euro_value: '',
+        notification_email: '',
+        google_recaptcha_private_key: '',
+        strip_production_private_key: '',
+        strip_test_private_key: ''
+        };
+        return{
+            form
+        };
+    }
+}
+</script>
--- resources/js/Pages/Settings/index.vue
+++ resources/js/Pages/Settings/index.vue
@@ -1,92 +1,34 @@
 <template>
-    <basic-table
-        :data="settings.hotels"
-        :tableHeaders="[
-            {
-                title: 'Nombre',
-                key: 'name',
-            },
-            {    
-                title: 'Correo',
-                key: 'email',
-            },
-            {    
-                title: 'Teléfono',
-                key: 'phone_number',
-            },
-            {    
-                title: 'Dirección',
-                key: 'address',
-            },
-            {    
-                title: 'Fecha de creación',
-                key: 'created_at',
-            },
-        ]"
-        table_id="settingsTable"
-        moduleName="settings"
-        detailsBtn
-    >
-        <template #name="element">
-            <div class="d-flex align-items-center">
-                <div class="flex-shrink-0 me-3">
-                    <div class="avatar-sm bg-light rounded p-1" style="overflow: hidden;">
-                        <img :src="element.cover?.full_path ?? getAvatar(element.name)" 
-                        alt="" class="img-fluid d-block">
-                    </div>
-                </div>
-                <div class="flex-grow-1">
-                    <h5 class="fs-14 mb-1">
-                        {{ element.name }}
-                    </h5>
-                    <p class="text-muted mb-0" 
-                        v-if="element"
-                    >
-                       {{ element.slug }}
-                    </p>
-                </div>
-            </div>
-        </template>
-        <template #address="element">
-            <div 
-                style="
-                    word-wrap: break-word;
-                    min-width: 160px;
-                    max-width: 160px;
-                    white-space:normal;
-                "
-            >
-                {{ element.address }}
-            </div>
-        </template>
-        <template #tableActions="element">
-            <btn-option
-                :btnRoute="element.url_address"
-                type="listButton"
-                color="info"
-                text="Url de dirección"
-                icon="ri-map-pin-2-fill"
-                v-if="can('settings.get')"
-            ></btn-option>
-        </template>
-    </basic-table>
-
-    <Settings-add-edit />
+<SettingEdit/>
 </template>
 
 <script>
-import BasicTable from '@/Components/BasicTable.vue'
-import BtnOption from '@/Components/BtnOption.vue'
-//import SettingAddEdit from '../../Components/Settings/settingAddEdit.vue';
+
+import BasicInput from '@/Components/BasicInput.vue'
+import SettingAddEdit from '@/Components/Settings/SettingEdit.vue';
 export default {
     components: {
-        BasicTable,
-        BtnOption,
+        SettingAddEdit,
+        BasicInput
     },
     props:{
-        settings: Object,
+       
     },
     setup(props){
+        const form = {
+        google_recaptcha_public_key: '',
+        strip_production_public_key: '',
+        strip_test_public_key: '',
+        dollar_value: '',
+        euro_value: '',
+        notification_email: '',
+        google_recaptcha_private_key: '',
+        strip_production_private_key: '',
+        strip_test_private_key: ''
+        };
+        return{
+            form
+        };
     }
 }
 </script>
