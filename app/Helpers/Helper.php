f332f919e166a2719a78b3e83f2df026f3e9db61
joel <joel.uss1728@gmail.com>
2023-09-21 08:33:01 -0700

    helper de badge contactos pendientes

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Contact;
+use Illuminate\Support\Facades\Auth;
+
+class Helper{
+
+    public static function get_current_branch_pending_contacts(){
+        
+        $pending_contact_count = Contact::when(!is_null(Auth::user()->hotel_id), function($q){
+            $q->where('hotel_id', Auth::user()->hotel_id);
+        })->where('status', 'pendiente')
+        ->count();
+    
+        return $pending_contact_count;
+    }
+}
