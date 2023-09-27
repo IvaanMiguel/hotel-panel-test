e490c40b64d90bc2dcf256ffa28298ce7c5cc207
J4viscript <tezjavi@gmail.com>
2023-09-26 18:40:50 -0700

    Contactos Listo

--- resources/js/Pages/Contacts/Index.vue
+++ resources/js/Pages/Contacts/Index.vue
@@ -6,22 +6,21 @@
           <h4 class="fw-medium">Filtros</h4>
           <ul class="list-unstyled">
             <li class="fw-medium p-2 cursor-pointer" @click="filterContacts('pendiente')"
-              :class="{ 'disabled': selectedStatus === 'pendiente', 'selected': selectedStatus === 'pendiente' }">
+              :class="{ 'disabled': selectedStatus === 'pendiente', 'selected': selectedStatus === 'pendiente', [statusColors.pendiente]: selectedStatus === 'pendiente' }">
               Pendientes
             </li>
             <li class="fw-medium p-2 cursor-pointer" @click="filterContacts('atendido')"
-              :class="{ 'disabled': selectedStatus === 'atendido', 'selected': selectedStatus === 'atendido' }">
+              :class="{ 'disabled': selectedStatus === 'atendido', 'selected': selectedStatus === 'atendido', [statusColors.atendido]: selectedStatus === 'atendido' }">
               Atendidos
             </li>
             <li class="fw-medium p-2 cursor-pointer" @click="filterContacts('archivado')"
-              :class="{ 'disabled': selectedStatus === 'archivado', 'selected': selectedStatus === 'archivado' }">
+              :class="{ 'disabled': selectedStatus === 'archivado', 'selected': selectedStatus === 'archivado', [statusColors.archivado]: selectedStatus === 'archivado' }">
               Archivados
             </li>
             <li class="fw-medium p-2 cursor-pointer" @click="filterContacts('all')"
-              :class="{ 'disabled': selectedStatus === 'all' }">
-              Limpiar Filtros
-            </li>
-            <!-- Agrega la opción para filtrar por hotel -->
+            :class="{ 'disabled': selectedStatus === 'all', 'selected': selectedStatus === 'all', 'badge': selectedStatus === 'all' }">
+            Todos
+          </li>
             <li class="fw-medium p-2" :class="{ 'disabled': selectedStatus === 'hotel' }">
               Filtrar por Hotel
               <select v-model="selectedHotel" class="form-select mt-2" @change="handleHotelChange">
@@ -44,6 +43,17 @@
         {
           title: 'Estado',
           key: 'status',
+          dataStyle: (element) => {
+            if (element.status === 'pendiente') {
+              return statusColors.pendiente;
+            } else if (element.status === 'archivado') {
+              return statusColors.archivado;
+            } else if (element.status === 'atendido') {
+              return statusColors.atendido; 
+            } else {
+              return '';
+            }
+          }
         },
         {
           title: 'Actualización',
@@ -68,9 +78,8 @@
           </div>
         </template>
       </basic-table>
+      <contacts-add-edit/>
     </div>
-
-    <contacts-add-edit />
   </div>
 </template>
 
@@ -92,24 +101,29 @@ export default {
   setup(props) {
     const filteredContacts = ref([]);
     const selectedStatus = ref('pendiente');
-    const selectedHotel = ref(''); // Agregar una referencia para el hotel seleccionado
+    const selectedHotel = ref('');
 
-    // Utiliza .map() para extraer el objeto `hotels` del arreglo `contacts`
     const contacts = props.variables.contacts || [];
     const hotels = contacts.map((contact) => contact.hotel);
 
-    // Utiliza un conjunto para obtener hoteles únicos
-    const uniqueHotelsSet = new Set(hotels);
-    const uniqueHotels = Array.from(uniqueHotelsSet);
-
-    console.log(uniqueHotels);
+    const uniqueHotels = contacts.reduce((unique, contact) => {
+      const hotelId = contact.hotel.id;
+      if (!unique.find(h => h.id === hotelId)) {
+        unique.push(contact.hotel);
+      }
+      return unique;
+    }, []);
+    const statusColors = {
+      pendiente: 'badge bg-danger', 
+      atendido: 'badge bg-success', 
+      archivado: 'badge bg-primary',
+    };
 
     const filterContacts = (status) => {
       if (props.variables?.contacts) {
         if (status === 'all') {
           filteredContacts.value = props.variables.contacts;
         } else if (status === 'hotel') {
-          // Filtra por hotel seleccionado
           filteredContacts.value = props.variables.contacts.filter(
             (contact) => contact.hotel.id === selectedHotel.value
           );
@@ -121,17 +135,15 @@ export default {
         selectedStatus.value = status;
       }
     };
-
-    // Maneja el cambio en la selección del hotel
     const handleHotelChange = () => {
       filterContacts('hotel');
     };
 
     watch(() => props.variables.contacts, () => {
       if (selectedStatus.value === 'hotel') {
-        filterContactsByHotel(selectedHotel.value); // Vuelve a aplicar el filtro de hotel si se seleccionó esta opción
+        filterContactsByHotel(selectedHotel.value);
       } else {
-        filterContacts(selectedStatus.value); // Vuelve a aplicar el filtro por estado
+        filterContacts(selectedStatus.value);
       }
     });
 
@@ -147,18 +159,13 @@ export default {
       filterContacts,
       ContactsAddEdit,
       uniqueHotels,
-      handleHotelChange, // Agrega la función handleHotelChange al objeto retornado
+      handleHotelChange,
+      statusColors,
     };
   },
 };
 </script>
 
-<style scoped>
-.selected {
-  background-color: black;
-  color: white;
-}
-</style>
 <style scoped>
 .selected {
   background-color: black;
