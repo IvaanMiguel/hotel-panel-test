1fe3466308119b8859510fe7f30741353899e260
Luis Pablo <racheth14@gmail.com>
2023-10-16 19:16:49 -0700

    Details Clients - Diseño boton edit

--- resources/js/Pages/Clients/showclient.vue
+++ resources/js/Pages/Clients/showclient.vue
@@ -55,7 +55,7 @@
                             </li>
                         </ul>
                         <btn-option :action="{ id: 'clients', method: 'edit', params: { id: clients.id } }"
-                            type="listButton" color="primary" text="Editar foto" icon="ri-image-edit-line"
+                        type="button" color="success" text="Editar foto" icon="ri-image-edit-line"
                             v-if="(can('clients.update'))"></btn-option>
                             
                     </div>
