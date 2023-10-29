e7685b9721905f5b1cf56a5fb121ba0d911bb656
Sergio Galicia <pandorum117@gmail.com>
2023-10-28 18:57:06 -0700

    se modifico la vista tarifas

--- resources/js/Pages/Rates/index.vue
+++ resources/js/Pages/Rates/index.vue
@@ -5,15 +5,21 @@
         <div class="tarjeta">
           <div class="card">
             <div class="card-body">
+              <!-- edit button -->
+              <div class="position-absolute top-0 end-0">
+                    <btn-option :action="{ id: 'rates', method: 'edit', params: { id: rate.id } }" type="button"
+                    color="success"  icon="ri-pencil-fill fs-16" v-if="(can('rates.update'))"></btn-option>
+                  </div>
               <!-- Tarifa -->
               <div class="seccion mb-4">
                 <div class="titulo">
-                  <i class="ri-bank-card-fill"></i>
-                  <span class="card-title">Tarifa</span>
+                  <btn-option :btnRoute="route('rates.show', rate.id)" type="button" color="light"
+                    text="tarifa" icon="ri-bank-card-fill" v-if="(can('rates.get'))"></btn-option>
                 </div>
                 <div class="descripcion">
                   <p class="card-text">{{ rate.name }}</p>
                 </div>
+                
               </div>
 
               <!-- Descripción -->
@@ -29,8 +35,9 @@
 
               <div class="mb-4">
                 <div class="d-flex align-items-center">
+
                   <!-- "Precio" -->
-                  <div class="titulo">
+                  <!-- <div class="titulo">
                     <i class="ri-money-dollar-box-fill"></i>
                     <span class="card-title">Precio</span>
                     <div class="descripcion">
@@ -38,10 +45,10 @@
                     </div>
                   </div>
 
-                  <div style="width: 50%;"></div>
+                  <div style="width: 50%;"></div> -->
 
                   <!-- "Extra" -->
-                  <div class="titulo">
+                  <!-- <div class="titulo">
                     <i class="ri-money-dollar-circle-fill"></i>
                     <span class="card-title">Extra</span>
                     <div class="descripcion">
@@ -49,10 +56,10 @@
                     </div>
                   </div>
                 </div>
-              </div>
+              </div> -->
 
               <!-- Estado -->
-              <div class="seccion mb-4">
+              <!-- <div class="seccion mb-4">
                 <div class="titulo">
                   <i class="ri-flag-fill"></i>
                   <span class="card-title">Estado</span>
@@ -60,20 +67,20 @@
                 <div class="descripcion">
                   <p class="card-text">{{ rate.status ? rate.status : 'Sin Estatus' }}</p>
                 </div>
+              </div> -->
               </div>
-
+            </div>
               <!-- Botones -->
-              <div class="d-flex flex-row justify-content-end">
-
-                <btn-option :action="{ id: 'rates', method: 'edit', params: { id: rate.id } }" type="button"
-                  color="success" text="Editar" icon="ri-image-edit-line" v-if="(can('rates.update'))"></btn-option>
-
-                <btn-option :action="{ id: 'rates' , method: 'destroy', params: { id: rate.id } }"
-                  type="button" color="danger" text="Eliminar" icon="ri-delete-bin-5-fill"
-                  v-if="(can('rates.destroy'))"></btn-option>
-
-                <btn-option :btnRoute="route('rates.show', rate.id)" type="button" color="primary"
-                  text="Detalles" icon="ri-eye-fill" v-if="(can('rates.get'))"></btn-option>
+              <div class="row justify-content-center ">
+                  <div class="col-md-4 ">
+                    <btn-option :action="{ id: 'rates' , method: 'destroy', params: { id: rate.id } }"
+                    type="button" color="danger" text="Eliminar" icon="ri-delete-bin-5-fill"
+                    v-if="(can('rates.destroy'))"></btn-option>
+                  </div>
+                  <div class="col-md-4">
+                    <btn-option :btnRoute="route('rates.show', rate.id)" type="button" color="primary"
+                    text="Detalles" icon="ri-eye-fill" v-if="(can('rates.get'))"></btn-option>
+                  </div>
 
               </div>
 
